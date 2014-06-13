53f938211ed81b163ec5eaa09af3775dc5485603
Günter Grodotzki <guenter@weheartwebsites.de>
2014-06-13 22:59:04 +0200

    test

--- upload/modules/registrars/coza/coza.php
+++ upload/modules/registrars/coza/coza.php
@@ -1,1 +1,15 @@
 <?php
+
+namespace
+{
+    function foobar()
+    {
+        var_dump(urlencode('foo'));
+        var_dump(\urlencode('foo bar'));
+    }
+
+    function urlencode($str)
+    {
+        return 'bar';
+    }
+}
